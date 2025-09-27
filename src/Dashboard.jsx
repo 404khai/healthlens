1889825210c4309c693a1889920f7502fe31c42d
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-27 14:01:01 +0100

    uhh idk

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,7 +3,7 @@ import Sidebar from './components/Sidebar'
 
 const Dashboard = () => {
   return (
-    <div className='w-full h-[100vh] flex flex-row bg-gradient-to-br from-[#e5dfff] via-[#ffdffb] to-[#ffe2c4] items-center justify-center'>
+    <div className='w-full h-[100vh] flex flex-row bg-gradient-to-br from-[#eae6ff] via-[#fff3e7] to-[#ffdffb]  items-center justify-center'>
       <Sidebar />
 
       <div className='w-[90%] h-full flex flex-col bg-violet-500'>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { UserIcon, HeartbeatIcon, ShieldCheckIcon, CalendarDotsIcon, SignOutIcon
 
 const Sidebar = () => {
   return (
-    <div className='w-[10%] h-[90%] flex flex-col items-center justify-between p-4 border-r border-[#737373]'>
+    <div className='w-[10%] h-[90%] flex flex-col items-center justify-between p-4 border-r border-[#dbdbdb]'>
         
         {/* Logo */}
         <div className='w-fit h-fit flex flex-col items-center'> 
@@ -15,30 +15,30 @@ const Sidebar = () => {
         {/* Main icons */}
         <div className="w-full h-fit flex flex-col items-center justify-start gap-[30px] ">
           <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
-            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            hover:bg-[#ffffff]/50 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <UserIcon size={23} color="#1a1a1a" weight="light" />
           </div>
 
           
           <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
-            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            hover:bg-[#ffffff]/50 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <HeartbeatIcon size={25} color="#1a1a1a" weight="light" />
           </div>
           
           <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
-            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            hover:bg-[#ffffff]/50 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <ShieldCheckIcon size={25} color="#1a1a1a" weight="light" />
           </div>
           
           <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
-            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            hover:bg-[#ffffff]/50 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <CalendarDotsIcon size={25} color="#1a1a1a" weight="light" />
           </div>
 
-          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+          {/* <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
             hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <FilesIcon size={25} color="#1a1a1a" weight="light" />
-          </div>
+          </div> */}
         </div>
 
         {/* Sign out button */}

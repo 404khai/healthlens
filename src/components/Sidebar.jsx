03c431eefd4127af24c73b2dc63350da7d98a28b
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-27 13:48:38 +0100

    Sidebar design contd

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-import { UserIcon, HeartbeatIcon, ShieldCheckIcon, CalendarDotsIcon, SignOutIcon } from "@phosphor-icons/react";
+import { UserIcon, HeartbeatIcon, ShieldCheckIcon, CalendarDotsIcon, SignOutIcon, FilesIcon } from "@phosphor-icons/react";
 
 const Sidebar = () => {
   return (
     <div className='w-[10%] h-[90%] flex flex-col items-center justify-between p-4 border-r border-[#737373]'>
         
         {/* Logo */}
-        <div className='w-full h-5 flex items-center mb-6'>
+        <div className='w-fit h-fit flex flex-col items-center'> 
             <img src={logo} alt="logo" className='w-full h-full' />
+            <p>HealthLens</p>
         </div>
 
         {/* Main icons */}
-        <div className="w-full h-[40%] flex flex-col items-center justify-start gap-[50px]">
-          <div className="p-2 rounded-xl cursor-pointer hover:bg-[#fff] hover:scale-110 transition-all">
-            <UserIcon size={25} color="#1a1a1a" weight="light" />
+        <div className="w-full h-fit flex flex-col items-center justify-start gap-[30px] ">
+          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            <UserIcon size={23} color="#1a1a1a" weight="light" />
           </div>
+
           
-          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <HeartbeatIcon size={25} color="#1a1a1a" weight="light" />
           </div>
           
-          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <ShieldCheckIcon size={25} color="#1a1a1a" weight="light" />
           </div>
           
-          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
             <CalendarDotsIcon size={25} color="#1a1a1a" weight="light" />
+          </div>
+
+          <div className="p-[5px] cursor-pointer rounded-[10px] transition-all hover:scale-110 
+            hover:bg-[#ffffff]/30 hover:backdrop-blur-md hover:border hover:border-[#ffffff]/30">
+            <FilesIcon size={25} color="#1a1a1a" weight="light" />
           </div>
         </div>
 

c8fb80a88badf35b2910f7cd004107b4b8e56a2e
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-27 12:50:15 +0100

    Sidebar, background design

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,12 +3,12 @@ import Sidebar from './components/Sidebar'
 
 const Dashboard = () => {
   return (
-    <div className='w-full h-full flex flex-row bg-[#4dbf0c]'>
-        <Sidebar />
+    <div className='w-full h-[100vh] flex flex-row bg-gradient-to-br from-[#e5dfff] via-[#ffdffb] to-[#ffe2c4] items-center justify-center'>
+      <Sidebar />
 
-        <div className='w-[90%] h-full flex flex-col bg-red'>
+      <div className='w-[90%] h-full flex flex-col bg-violet-500'>
 
-        </div>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import logo from '../assets/logo.jpeg'
-import { HeartbeatIcon } from "@phosphor-icons/react";
+import logo from '../assets/logo.png'
+import { UserIcon, HeartbeatIcon, ShieldCheckIcon, CalendarDotsIcon, SignOutIcon } from "@phosphor-icons/react";
 
 const Sidebar = () => {
   return (
-    <div className='w-[10%] h-full bg-sky-500 flex flex-col items-center justify-center p-4 border-r border-gray-300'>
-        <div className='w-10 h-10 flex items-center'>
-            <img src={logo} alt="" className='w-5 h-5 '/>
+    <div className='w-[10%] h-[90%] bg-sky-500 flex flex-col items-center justify-between p-4 border-r border-gray-300'>
+        <div className='w-full h-10 flex bg-gray-100 items-center'>
+            <img src={logo} alt="" className='w-full h-full '/>
         </div>
 
-        <div className='flex flex-col items-center justify-center gap-6'>
-            <HeartbeatIcon size={32} color="#FF0000" weight="fill" className='cursor-pointer hover:scale-110 transition-transform'/>
+        <div className='bg-gray-50 w-full h-[50%] flex flex-col items-center justify-between gap-6 mb-10'>
+          <UserIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+          <HeartbeatIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+          <ShieldCheckIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+          <CalendarDotsIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+          <HeartbeatIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
         </div>
+
+        <SignOutIcon size={32} color="#FF0000" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
     </div>
   )
 }

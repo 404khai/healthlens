004d70fd87617a608482f1e0251d6c81c9724dff
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-27 13:13:56 +0100

    Sidebar design, tailwind issues

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,36 @@ import { UserIcon, HeartbeatIcon, ShieldCheckIcon, CalendarDotsIcon, SignOutIcon
 
 const Sidebar = () => {
   return (
-    <div className='w-[10%] h-[90%] bg-sky-500 flex flex-col items-center justify-between p-4 border-r border-gray-300'>
-        <div className='w-full h-10 flex bg-gray-100 items-center'>
-            <img src={logo} alt="" className='w-full h-full '/>
+    <div className='w-[10%] h-[90%] flex flex-col items-center justify-between p-4 border-r border-[#737373]'>
+        
+        {/* Logo */}
+        <div className='w-full h-5 flex items-center mb-6'>
+            <img src={logo} alt="logo" className='w-full h-full' />
         </div>
 
-        <div className='bg-gray-50 w-full h-[50%] flex flex-col items-center justify-between gap-6 mb-10'>
-          <UserIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
-          <HeartbeatIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
-          <ShieldCheckIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
-          <CalendarDotsIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
-          <HeartbeatIcon size={25} color="#555" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+        {/* Main icons */}
+        <div className="w-full h-[40%] flex flex-col items-center justify-start gap-[50px]">
+          <div className="p-2 rounded-xl cursor-pointer hover:bg-[#fff] hover:scale-110 transition-all">
+            <UserIcon size={25} color="#1a1a1a" weight="light" />
+          </div>
+          
+          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+            <HeartbeatIcon size={25} color="#1a1a1a" weight="light" />
+          </div>
+          
+          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+            <ShieldCheckIcon size={25} color="#1a1a1a" weight="light" />
+          </div>
+          
+          <div className="p-2 rounded-xl cursor-pointer hover:bg-white hover:scale-110 transition-all">
+            <CalendarDotsIcon size={25} color="#1a1a1a" weight="light" />
+          </div>
         </div>
 
-        <SignOutIcon size={32} color="#FF0000" weight="light" className='cursor-pointer hover:scale-110 transition-transform'/>
+        {/* Sign out button */}
+        <div className="p-2 rounded-xl cursor-pointer hover:bg-red-100 hover:scale-110 transition-all">
+          <SignOutIcon size={25} color="#FF0000" weight="light" />
+        </div>
     </div>
   )
 }
